82fdbe20fbe570b1adbb3c9b409f01a80acc2984
fateev <fateev@COMP-200>
2020-07-26 17:25:30 +0300

    Color change by btns 2

--- WindowsFormsApp2/Form1.cs
+++ WindowsFormsApp2/Form1.cs
@@ -13,40 +13,26 @@ namespace WindowsFormsApp2
     public partial class Form1 : Form
     {
         static int count = 0;
-        Timer timer;
+        Color[] colors = {
+        Color.Red,
+        Color.Firebrick,
+        Color.AliceBlue,
+        Color.Cyan,
+        Color.DarkSalmon
+        };
         public Form1()
         {
             InitializeComponent();
-            timer = new Timer();
-            timer.Tick += Timer_Tick;
-            timer.Start();
-
-
-            button1.Tag = "run_timer";
-        }
-
-        private void Timer_Tick(object sender, EventArgs e)
-        {
-            this.BackColor = (++count % 2 == 0) ? Color.Red : Color.Blue;
-            this.Text = count.ToString();
-            System.Threading.Thread.Sleep(500);
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
+            this.BackColor = colors[--count];
+        }
 
-            if (button1.Tag.Equals("stop_timer"))
-            {
-                timer.Start();
-                button1.Tag  = "run_timer";
-                button1.Text  = "stop_timer";
-            }
-            else if (button1.Tag.Equals("run_timer"))
-            {
-                timer.Stop();
-                button1.Tag = "stop_timer";
-                button1.Text = "run_timer";
-            }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.BackColor = colors[++count];
         }
     }
 }
