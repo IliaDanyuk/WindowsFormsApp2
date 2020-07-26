71c5d485183fc79ecd1061bf9335901a5ddbae52
fateev <fateev@COMP-200>
2020-07-26 17:00:55 +0300

    Add timer start / stop bg color

--- WindowsFormsApp2/Form1.cs
+++ WindowsFormsApp2/Form1.cs
@@ -13,15 +13,38 @@ namespace WindowsFormsApp2
     public partial class Form1 : Form
     {
         static int count = 0;
+        Timer timer;
         public Form1()
         {
             InitializeComponent();
+            timer = new Timer();
+            timer.Tick += Timer_Tick;
+            timer.Start();
+
+
+            button1.Tag = "run_timer";
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            this.BackColor = (++count % 2 == 0) ? Color.Red : Color.Blue;
+            this.Text = count.ToString();
+            System.Threading.Thread.Sleep(500);
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            this.BackColor =  (++count % 2 == 0 ) ?  Color.Red : Color.Blue;
-            this.Text = count.ToString();
+
+            if (button1.Tag.Equals("stop_timer"))
+            {
+                timer.Start();
+                button1.Tag = "run_timer";
+            }
+            else if (button1.Tag.Equals("run_timer"))
+            {
+                timer.Stop();
+                button1.Tag = "stop_timer";
+            }
         }
     }
 }
