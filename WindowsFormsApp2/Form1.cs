eb9eb5dceb88493f6470f964bfdf34a581525223
fateev <fateev@COMP-200>
2020-07-26 16:48:01 +0300

    Change color by btn press (red or blue)

--- WindowsFormsApp2/Form1.cs
+++ WindowsFormsApp2/Form1.cs
@@ -12,9 +12,16 @@ namespace WindowsFormsApp2
 {
     public partial class Form1 : Form
     {
+        static int count = 0;
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.BackColor =  (++count % 2 == 0 ) ?  Color.Red : Color.Blue;
+            this.Text = count.ToString();
         }
     }
 }
